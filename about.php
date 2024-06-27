1356ba8ac2b1ad1ee948c025dd892948eefe92fa
V3en-us <venus.kabera@strathmore.edu>
2024-06-27 18:02:12 +0300

    changed donate.html to php

--- about.php
+++ about.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./CSS/style.css">
+</head>
+<body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">ADEASE</a>
@@ -14,31 +17,28 @@
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/index.html">Home</a>
+                <a class="nav-link active" aria-current="page" href="/index.php">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./Sign-up.html">Sign-up</a>
+                <a class="nav-link active" aria-current="page" href="./Sign-up.php">Sign-up</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./patners.html">Partners</a>
+                <a class="nav-link" href="./partners.php">Partners</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./Contact-us.html">Contact</a>
+                <a class="nav-link" href="./Contact-us.php">Contact</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./help.html">Help</a>
+                <a class="nav-link" href="./help.php">Help</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./about.html">About</a>
+                <a class="nav-link" href="./about.php">About</a>
               </li>
-             
             </ul>
           </div>
         </div>
       </nav>
-    <link rel="stylesheet" href="./CSS/style.css">
-</head>
-<body>
+
    <h1>About Us</h1> 
    <p>Lorem ipsum doloor sit, amet consectetur adipisicing elit. Laboriosam eligendi porro quos dignissimos rerum tempore, autem ratione nulla repudiandae quo eos, voluptatibus, quasi saepe possimus rem maxime laborum sapiente illo.</p>
 
