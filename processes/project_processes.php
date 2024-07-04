5682354d1879a4f3903410d78873f8cc5bfff4bf
V3en-us <venus.kabera@strathmore.edu>
2024-07-04 20:36:49 +0300

    working on table 2 b4 sign in and sign out

---
+++ processes/project_processes.php
@@ -0,0 +1,22 @@
+<?php
+// projectInsert.php
+require_once "dbConnect.php";
+
+if (isset($_POST["submit"])) {
+    $projectname = mysqli_real_escape_string($dbConn, $_POST["projectname"]);
+    $description = mysqli_real_escape_string($dbConn, $_POST["description"]);
+    $started = mysqli_real_escape_string($dbConn, $_POST["started"]);
+    $completed = mysqli_real_escape_string($dbConn, $_POST["completed"]);
+
+    $insertProject = "
+    INSERT INTO projects (projectname, description, started, completed) 
+    VALUES ('$projectname', '$description', '$started', '$completed')";
+
+    if ($dbConn->query($insertProject) === TRUE) {
+        echo "New project record created successfully";
+        header("Location: ../sign-in.php"); // Redirect to a different page
+    } else {
+        die("Failed to insert the new project record: " . $dbConn->error);
+    }
+}
+?>
