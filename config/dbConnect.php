22573489fc46abfaee41f3b79f25f1a122bed9c9
V3en-us <venus.kabera@strathmore.edu>
2024-07-04 13:29:43 +0300

    db connected

---
+++ config/dbConnect.php
@@ -0,0 +1,15 @@
+<?php
+// Inserting constants content
+require_once "Constants.php";
+
+// Create the database connection
+$dbConn = new mysqli(HOSTNAME, HOSTUSER, HOSTPASS, DBNAME);
+
+// Verify the connection
+if($dbConn->connect_error){
+    die("Connection Failed: " . $dbConn->connect_error);
+}else{
+    echo "The connection was successful!!!";
+}
+http://localhost/first-repo/config/dbConnect.php
+?>
