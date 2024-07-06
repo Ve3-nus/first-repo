fd056daed7a558fc060754aea4ebd6fd1e8fc78f
V3en-us <venus.kabera@strathmore.edu>
2024-07-06 10:17:21 +0300

    corrected the navbar

--- index.php
+++ index.php
@@ -18,25 +18,26 @@
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/index.html">Home</a>
+                <a class="nav-link active" aria-current="page" href="/index.php">Home</a>
+              </li>
+             
+              <li class="nav-item">
+                <a class="nav-link" href="./patners.php">Partners</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./Sign-up.html">Sign-up</a>
+                <a class="nav-link" href="./Contact-us.php">Contact</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./patners.html">Partners</a>
+                <a class="nav-link" href="./donate.php">Donate</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./Contact-us.html">Contact</a>
+                <a class="nav-link" href="./help.php">Help</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./donate.html">Donate</a>
+                <a class="nav-link" href="./about.php">About</a>
+                <li class="nav-item">
+                <a class="nav-link" href="./services.php">Projects</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./help.html">Help</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./about.html">About</a>
               </li>
              
             </ul>
